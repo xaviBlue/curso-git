e6a4d3ef58e0ef2551e4fa2deecce16804437657
XaviArcia <xarcia@est.ups.edu.ec>
2020-11-20 13:33:04 -0500

    Tipos de datos

---
+++ datatype.py
@@ -0,0 +1,45 @@
+#String
+print("Hello world")
+print('''HELLO WORLD''')
+print('Hello WOrld')
+print("""Hello World""")
+
+print(type("Hello world"))
+
+print("Bye"+" World")
+
+#Numbers
+print(100)
+print(type(100.1))
+
+#Float
+print(12.3)
+print(type(30.2))
+
+#Boolean
+print(False)
+print(type(False))
+
+#List
+print([10,20,30,40])
+print(['Hello','Bye','Adios'])
+print(type([10,"Hola",True,10.1]))
+[]
+
+#Tuplas
+print(10,20,30,40)
+print(type((19,29,21,1)))
+
+#Diccionario
+print({
+    "nombre":"Ryan",
+    "apellido":"Ray",
+    "apodo":"Fazt"
+})
+print(type({
+    "lat":123.2,
+    "alt":23.87
+}))
+
+#None
+print(type(None))
