45146b327b216c01fbd757a616a6461b10bc516c
XaviArcia <xarcia@est.ups.edu.ec>
2020-11-20 14:20:47 -0500

    Variables

---
+++ variables.py
@@ -0,0 +1,20 @@
+name = "Xavier"
+print(name)
+
+number=100
+print(number)
+
+x=100
+book="Juntos Solos"
+print(x, book)
+
+#Convenciones
+book_name="El principito"
+bookName="Hola"
+BookName="Estoy bien"
+
+#Constante
+PI = 3.1416 #Mayusculas
+
+book_name="I robot"
+book_name=121112
