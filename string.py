ba34deb68a0864e9aceb58727bad54099c8d6fe8
XaviArcia <xarcia@est.ups.edu.ec>
2020-11-21 02:34:16 -0500

    Strings

---
+++ string.py
@@ -0,0 +1,33 @@
+my_str="Hello world"
+
+print(my_str + ' Welcome my friend')
+print(f'{my_str} Welcome my friend')
+print('{0} Welcome my friend'.format(my_str))
+
+#print(dir(my_str))
+
+# print(my_str.upper())
+# print(my_str.lower())
+# print(my_str.swapcase())
+# print(my_str.capitalize())
+# #Reemplazar la cadena de caracteres
+# print(my_str.replace('Hello','bye'))
+# print(my_str.replace('Hello','bye').upper())
+# #Contar cuantas veces existe el caracter
+# print(my_str.count(' '))
+# #buscar por caracter 
+# print(my_str.startswith('hola'))
+# print(my_str.endswith('world'))
+# #Separar a travez de un caracter
+# print(my_str.split('e'))
+# #Posicion del caracter - indice
+# print(my_str.find('e'))
+# #Tamaño string
+# print(len(my_str))
+# #bucar indice directamente
+# print(my_str.index('e'))
+# #saber si es un tipo de dato
+# print(my_str.isnumeric())
+# print(my_str.isalpha())
+# #Valor que quiero ver en el indice -1 inversa
+# print(my_str[3])
