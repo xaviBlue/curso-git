538b4638f9ed1b580855fc3f6cb9eec9aaeaf51d
XaviArcia <xarcia@est.ups.edu.ec>
2020-11-24 01:23:03 -0500

    Dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,16 @@
+product={
+    "name":"book",
+    "quantity":3,
+    "price":4.99
+}
+person={
+    "name":"brook",
+    "age":3,
+    "altura":54.99
+}
+
+print(dir(person))
+print(person.keys())
+
+person.clear()
+print(person)
