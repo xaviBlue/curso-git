d2ef33decf0c8b5b176657f747f8679b3bfff7c0
XaviArcia <xarcia@est.ups.edu.ec>
2020-11-24 01:06:19 -0500

    Tuplas

---
+++ tuples.py
@@ -0,0 +1,13 @@
+x = (1,2,3,4,5)
+print(type(x))
+
+y=tuple((1,2,3))
+print(y)
+
+print(x[0])
+#usamos del para eliminer la tupla
+locations={
+    (34.56,6431.4):"tokyo",
+    (34.6,641.4):"orlando"
+}
+print(locations)
