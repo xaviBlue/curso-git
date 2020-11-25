ee864b977c7f368fcdc106fae7c29b6d76bea70f
XaviArcia <xarcia@est.ups.edu.ec>
2020-11-25 02:17:12 -0500

    Bucles

---
+++ loops.py
@@ -0,0 +1,17 @@
+foods=['apples','bread','egg','milk','soja']
+
+for food in foods:
+    if food=='bread':
+        print('tienes que comprar esto')
+        #break
+        continue
+    print(food)
+
+
+for number in range(1,8):
+    print(number)
+
+count =4
+while count<=10:
+    print(count)
+    count=count +1
