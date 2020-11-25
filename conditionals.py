c1f9fcd5ac7daccbf3e61e63ddb6357ecc97d613
XaviArcia <xarcia@est.ups.edu.ec>
2020-11-25 02:03:06 -0500

    Condicionales

--- conditionals.py
+++ conditionals.py
@@ -4,4 +4,25 @@ if x < 30 :
     print("x es menor que 30")
 else:
     print("x es mayor que 20")
-        
+
+color ='blue'
+
+if color=='red':
+    print('El color es rojo')
+elif color == 'blue':
+    print('The color is blue')
+else:
+    print('es mi color')
+
+name = 'Xavier'
+lastame='Arcia'
+
+if name=='Xavier':
+    if lastame=='Arcia':
+        print('Tu eres: '+name+' '+lastame)
+    else:
+        print('Tu no eres Xavier')
+
+if x>2 and x<=10:
+    print('X es nayor a 2 y menor o igual que 10')
+
